d6e5fca3118597759f3883359ad0298a1fb0ed57
alesi arab <miverydisco@gmail.com>
2025-02-07 14:39:56 -0300

    Update page.tsx

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -7,7 +7,7 @@ export default async function EventListPage() {
       <section className="md:container pb-8 m-auto">
         <div className="m-auto flex max-w-[950px] flex-col items-start gap-2">
           <h1 className="mb-4 m-auto md:mx-0 text-3xl md:text-6xl font-display text-zinc-900 dark:text-zinc-100 leading-tight tracking-tighter">
-            Upcoming Events
+            Próximos Eventos
           </h1>
           <EventsList
             className="m-auto grid place-items-center grid-cols-1 md:grid-cols-2 xl:grid-cols-2 gap-8"
