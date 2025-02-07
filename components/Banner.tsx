7e192f63bfea72fc9757a75e5189462572e8115a
alesi arab <miverydisco@gmail.com>
2025-02-07 17:52:03 -0300

    Update Banner.tsx

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -4,7 +4,7 @@ export default function Banner(): JSX.Element {
   return (
     <div className="bg-zinc-100 text-center md:flex justify-center p-5 dark:bg-zinc-800 text-base">
       <div className="mt-2">
-        <span>Incorporate como coordinador/a solicitá una entrevista por whatsapp ahora</span>
+        <span>Incorporate como coordinador/a solicitándolo</span>
         <a
           href="https://wa.me/541157577039"
           className="text-[#25D366] dark:text-[#25D366] font-medium ml-2"
