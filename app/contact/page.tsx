574fcca268a10ee422f3750fd8b3877a71d74612
alesi arab <miverydisco@gmail.com>
2025-02-07 14:38:45 -0300

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,7 +6,7 @@ export default async function ContactPage() {
       <section className="pb-8 m-auto px-4">
         <div className="m-auto max-w-[950px] flex flex-col items-start gap-2">
           <h1 className="mb-4 m-auto md:mx-0 text-3xl md:text-6xl font-display text-zinc-900 dark:text-zinc-100 leading-tight tracking-tighter">
-            Contact
+            Contacto
           </h1>
         </div>
         <ContactForm className="w-full max-w-[500px] m-auto my-10" />
