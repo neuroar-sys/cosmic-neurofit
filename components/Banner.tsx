6697b0019520be01c5f26419f3dcb9f222ac059a
alesi arab <miverydisco@gmail.com>
2025-03-03 12:57:10 -0300

    Update Banner.tsx

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -3,7 +3,7 @@ export default function Banner(): JSX.Element {
     <div className="bg-zinc-100 text-center md:flex justify-center p-5 dark:bg-zinc-800 text-base">
       <div className="mt-2">
         <span>
-          Incorporate como coordinador/a, clic aquí {' '}
+          Incorporate como coordinador/a,{' '}
           <a
             href="https://wa.me/541157577039"
             className="text-[#25D366] dark:text-[#25D366] font-medium ml-2"
