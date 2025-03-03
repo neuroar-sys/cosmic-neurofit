387b194871032ec06f1f8550f437714d3bdbb646
alesi arab <miverydisco@gmail.com>
2025-03-03 12:53:59 -0300

    Update Banner.tsx

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,19 +1,19 @@
-// export default function Banner(): JSX.Element {
-//   return (
-//     <div className="bg-zinc-100 text-center md:flex justify-center p-5 dark:bg-zinc-800 text-base">
-//       <div className="mt-2">
-//         <span>
-//           Incorporate como coordinador/a, clic aquí{' '}
-//           <a
-//             href="https://wa.me/541157577039"
-//             className="text-[#25D366] dark:text-[#25D366] font-medium ml-2"
-//             aria-label="Contactar por WhatsApp"
-//           >
-//             WhatsApp
-//           </a>
-//           ahora.
-//         </span>
-//       </div>
-//     </div>
-//   );
-// }
+export default function Banner(): JSX.Element {
+  return (
+    <div className="bg-zinc-100 text-center md:flex justify-center p-5 dark:bg-zinc-800 text-base">
+      <div className="mt-2">
+        <span>
+          Incorporate como coordinador/a, clic aquí{' '}
+          <a
+            href="https://wa.me/541157577039"
+            className="text-[#25D366] dark:text-[#25D366] font-medium ml-2"
+            aria-label="Contactar por WhatsApp"
+          >
+            WhatsApp
+          </a>
+          ahora.
+        </span>
+      </div>
+    </div>
+  );
+}
